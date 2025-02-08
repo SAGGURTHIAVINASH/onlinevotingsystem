21e22b4923192903a782aa79ac45cb5a2c8e1ff2
SAGGURTHI.AVINASH <saggurthiavinash6@gmail.com>
2025-02-09 04:45:53 +0530

    Add files via upload

---
+++ otpscript.js
@@ -0,0 +1,90 @@
+const inputs = document.querySelectorAll("input"),
+button = document.querySelector("button"),
+mobile = document.getElementById("mobile"),
+expire = document.getElementById("expire");
+let OTP = "";
+expireInterval = "";
+function generateOTPs() {
+    OTP = 
+    Math.floor(Math.random() * 10) + 
+    "" +
+    Math.floor(Math.random() * 10) + 
+    "" +
+    Math.floor(Math.random() * 10) + 
+    "" + 
+    Math.floor(Math.random() * 10 );
+ alert("Your OTP is: " + OTP);
+inputs[0].focus();
+ expire.innerText = 120;
+ const expireInterval = setInterval(function () {
+    expire.innerText--;
+    if (expire.innerText == 0) {
+        clearInterval(expireInterval);
+    }
+ }, 1000); 
+}
+
+function clearOTPs() {
+    inputs.forEach((input, i) => {
+        input.value = "";
+        if(i == 0) {
+            input.removeAttribute("disabled");
+    
+        }
+        if (i != 0) {
+        input.setAttribute("disabled", true);
+        }
+    });
+clearInterval(expireInterval);
+expire.innerText = 0;
+button.setAttribute("disabled", true);
+button.classList.remove("active");
+}
+inputs.forEach((input, index) => {
+    input.addEventListener("keyup", function (e) {
+const currentInput = input,
+nextInput = input.nextElementSibling,
+prevInput = input.previousElementSibling;
+//console.log(currentIndex, nextInput, prevInput);
+   if(nextInput && nextInput.hasAttribute("disabled") &&
+currentInput.value!=="") {
+    nextInput.removeAttribute("disabled", true);
+    nextInput.focus();
+}
+if(e.key === "Backspace"){
+    inputs.forEach((input, index1)=>{
+        if(index <= index1 && prevInput) {
+            input.setAttribute("disabled", true);
+            prevInput.focus();
+            prevInput.value = "";
+        }
+    });
+}
+if (!inputs[3].disabled && inputs[3].value !== "") {
+    inputs[3].blur();
+    button.classList.add("active");
+    return;
+}
+button.classList.remove("active");
+});
+});
+window.addEventListener("load", () => {
+    let x = prompt("please enter your mobile number to verify your as voter");
+    if (x) {
+        mobile.innerText = x;
+generateOTPs();
+    }
+});
+button.addEventListener("click", () => {
+    let verify = '';
+    inputs.forEach((input) => {
+        verify += input.value;
+    });
+    if (verify === OTP) {
+        alert("Your OTP has been verified successfully!");
+        clearOTPs();
+    } else {
+alert("your verification Failed!");
+    }
+    
+});
